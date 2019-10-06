0de2773642171e264808664558c0ed73de047e5b
Benjamin Lannon <benjamin@lannonbr.com>
2019-10-06 16:16:38 -0400

    styling header to be blue

---
+++ src/gatsby-plugin-theme-ui/index.js
@@ -0,0 +1,9 @@
+import Theme from "gatsby-theme-github-stats/src/gatsby-plugin-theme-ui"
+
+export default {
+  ...Theme,
+  colors: {
+    ...Theme.colors,
+    headerBackground: "#23A9F2",
+  },
+}
